fd4f00c8d04f981aa595d44308f0dbfb273b60ca
KittyMagician <kittymagician@flobname.com>
2020-04-15 14:13:46 -0400

    removed the create_app from the context of the loop so that Gunicorn can import the app when creating a wscgi proxy.

--- app.py
+++ app.py
@@ -318,9 +318,8 @@ def create_app():
         conn.close()
         return str(resp)
     return app
-
+app = create_app()
 
 # Start development web server
 if __name__=='__main__':
-    app = create_app()
     app.run(host='0.0.0.0', port=5000, debug=False)
